d4ce4cdc64dcd491343707a840882f636f3ca40c
lovishchoudhary <lovishiit@gmail.com>
2014-10-09 11:59:21 +0530

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Lovish Choudhary"
+    email    "lovishiit@gmail.com"
+    password "foobars"
+    password_confirmation "foobars"
+  end
+end
